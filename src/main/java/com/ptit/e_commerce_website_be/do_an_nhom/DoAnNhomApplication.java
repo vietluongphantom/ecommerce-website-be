a30970bedadd82d27c51d43963fc356d5a90bdfc
Luongpro <email.vietluong@gmail.com>
2024-10-10 22:45:50 +0700

    first commit

---
+++ src/main/java/com/ptit/e_commerce_website_be/do_an_nhom/DoAnNhomApplication.java
@@ -0,0 +1,13 @@
+package com.ptit.e_commerce_website_be.do_an_nhom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnNhomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnNhomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptit/e_commerce_website_be/do_an_nhom/DoAnNhomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptit.e_commerce_website_be.do_an_nhom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnNhomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
