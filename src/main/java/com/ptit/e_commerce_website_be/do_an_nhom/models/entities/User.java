b7587b76b48620950dd4ea5d94e7f04ec3b7d6e9
khanhdev <phamquockhanhptit@gmail.com>
2024-10-13 19:44:10 +0700

    add entity user

---
+++ src/main/java/com/ptit/e_commerce_website_be/do_an_nhom/models/entities/User.java
@@ -0,0 +1,90 @@
+package com.ptit.e_commerce_website_be.do_an_nhom.models.entities;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column
+    private String phone;
+
+//    @Column
+//    private boolean status;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @JsonProperty("full_name")
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @ManyToMany(targetEntity = Role.class,
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    @JsonManagedReference
+    private Set<Role> roles;
+
+    @Column(name = "status", columnDefinition = "tinyint(1) default 0")
+    private Boolean status = false;// = Boolean.FALSE;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().toString()))
+                .collect(Collectors.toList());
+    }
+
+    @JsonProperty("address_id")
+    @Column(name = "address_id")
+    private Long addressId;
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
